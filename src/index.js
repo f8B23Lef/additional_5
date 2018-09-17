ae7faee1357bb7da3df866cd08127f0640f8610e
f8B23Lef <mal7rish@yandex.ru>
2018-09-17 23:51:45 +0300

    additional_5/fix: tab size

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
 
   var openArr = [];
-	var closeArr = [];
+  var closeArr = [];
   
-	for(var i = 0; i < bracketsConfig.length; i++) {
-		openArr.push(bracketsConfig[i][0]);
-		closeArr.push(bracketsConfig[i][1]);
-	}	
+  for(var i = 0; i < bracketsConfig.length; i++) {
+    openArr.push(bracketsConfig[i][0]);
+    closeArr.push(bracketsConfig[i][1]);
+  }	
 
-	var stack  = [];
-	var matchOpenBracket;
+  var stack  = [];
+  var matchOpenBracket;
 	
-	for(var k = 0; k < str.length; k++) {
+  for(var k = 0; k < str.length; k++) {
 
-		if(openArr.includes(str.charAt(k)) && !closeArr.includes(str.charAt(k))) { //opening and non-closing bracket
+    if(openArr.includes(str.charAt(k)) && !closeArr.includes(str.charAt(k))) { //opening and non-closing bracket
 
       stack.push(str.charAt(k));
 
@@ -29,12 +29,12 @@ module.exports = function check(str, bracketsConfig) {
 
     } else if(closeArr.includes(str.charAt(k))) { //closing bracket
 
-        matchOpenBracket = openArr[closeArr.indexOf(str.charAt(k))];  
+      matchOpenBracket = openArr[closeArr.indexOf(str.charAt(k))];  
         
-        if(stack.length == 0 || (stack.pop() != matchOpenBracket)) {
-				  return false;
-        }
-		}
+      if(stack.length == 0 || (stack.pop() != matchOpenBracket)) {
+        return false;
+      }
+    }
   }
 
   return stack.length == 0;
