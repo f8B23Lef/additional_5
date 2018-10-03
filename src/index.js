72def9581584bf2d2bb6e56f1e4872d6e1240fab
f8B23Lef <mal7rish@yandex.ru>
2018-10-03 18:45:27 +0300

    additional_5/fix: tab size

--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ module.exports = function check(str, bracketsConfig) {
  
   let tmpStr = str;
 	
-	for(let strKey = tmpStr.length-1; strKey >= 0; strKey--) {
-	  for(let bracketKey = 0; bracketKey < bracketsConfig.length; bracketKey++) {
-		  if(tmpStr[strKey] === bracketsConfig[bracketKey][0]) {
-			  tmpStr = tmpStr.replace(bracketsConfig[bracketKey].join(''), '');
-			  break;
-		  }
-	  }
+  for(let strKey = tmpStr.length-1; strKey >= 0; strKey--) {
+    for(let bracketKey = 0; bracketKey < bracketsConfig.length; bracketKey++) {
+      if(tmpStr[strKey] === bracketsConfig[bracketKey][0]) {
+        tmpStr = tmpStr.replace(bracketsConfig[bracketKey].join(''), '');
+        break;
+      }
+    }
   }
-  
   return tmpStr === '';
 }
