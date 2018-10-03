247088cf2f1ea60d355b7348a7b153fd3866f119
f8B23Lef <mal7rish@yandex.ru>
2018-10-03 18:41:02 +0300

    additional_5/refactor solution

--- src/index.js
+++ src/index.js
@@ -1,40 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-
-  const openArr = [];
-  const closeArr = [];
-  
-  for(let i = 0, len = bracketsConfig.length; i < len; i++) {
-    openArr.push(bracketsConfig[i][0]);
-    closeArr.push(bracketsConfig[i][1]);
-  }	
-
-  const stack  = [];
+ 
+  let tmpStr = str;
 	
-  for(let k = 0, len = str.length; k < len; k++) {
-
-    if(openArr.includes(str.charAt(k)) && !closeArr.includes(str.charAt(k))) { //opening and non-closing bracket
-
-      stack.push(str.charAt(k));
-
-    } else if(openArr.includes(str.charAt(k)) && closeArr.includes(str.charAt(k))) { //same opening and non-closing brackets
-      
-      if(stack.includes(str.charAt(k))) {
-        if(stack.pop() != str.charAt(k)) {
-          return false;
-        } 
-      } else {
-        stack.push(str.charAt(k));
-      }
-
-    } else if(closeArr.includes(str.charAt(k))) { //closing bracket
-
-      const matchOpenBracket = openArr[closeArr.indexOf(str.charAt(k))];  
-        
-      if(stack.length == 0 || (stack.pop() != matchOpenBracket)) {
-        return false;
-      }
-    }
+	for(let strKey = tmpStr.length-1; strKey >= 0; strKey--) {
+	  for(let bracketKey = 0; bracketKey < bracketsConfig.length; bracketKey++) {
+		  if(tmpStr[strKey] === bracketsConfig[bracketKey][0]) {
+			  tmpStr = tmpStr.replace(bracketsConfig[bracketKey].join(''), '');
+			  break;
+		  }
+	  }
   }
-
-  return stack.length == 0;
+  
+  return tmpStr === '';
 }
